755438cd2f6a1b9d7382ccb6db50e0a31c2871c4
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-07 20:24:56 -0500

    Updated test cases for vertical word adding

--- Drive.java
+++ Drive.java
@@ -9,11 +9,39 @@ public class Drive{
     System.out.println(grid.addWordHorizontal("hey", 0, 1)); //true;
     grid.addWordHorizontal("hey", 0, 1);
     System.out.println(grid);
+    /*Expected
+    _ h e y _
+    _ _ _ _ _
+    */
     System.out.println(grid.addWordHorizontal("dogma", 0, 1)); //false;
     grid.addWordHorizontal("dogma", 0, 1);
     System.out.println(grid); //no change
     System.out.println(grid.addWordHorizontal("hay", 0, 1)); //false;
     grid.addWordHorizontal("hay", 0, 1);
     System.out.println(grid); // no change
+    System.out.println(grid.addWordVertical("do", 0, 0)); //true
+    grid.addWordVertical("do", 0,0);
+    System.out.println(grid);
+    /*Expected
+    d h e y _
+    o _ _ _ _
+    */
+    System.out.println(grid.addWordVertical("ho", 0, 1)); //true
+    grid.addWordVertical("ho", 0,1);
+    System.out.println(grid);
+    /*Expected
+    d h e y _
+    o o _ _ _
+    */
+    System.out.println(grid.addWordVertical("hoo", 0, 1)); //false
+    grid.addWordVertical("hoo", 0,1);
+    System.out.println(grid); // no change
+    System.out.println(grid.addWordHorizontal("oomph", 1, 0)); //false
+    grid.addWordVertical("oomph", 1,0);
+    System.out.println(grid);
+    /*Expected
+    d h e y _
+    o o m p h
+    */
   }
 }
