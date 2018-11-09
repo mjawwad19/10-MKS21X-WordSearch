3a0b781d9a882affba8e3e0ee4e6478ac56a5eb8
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-09 18:29:05 -0500

    Updated personal driver to test new constructor

--- Drive.java
+++ Drive.java
@@ -1,47 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 public class Drive{
   public static void main(String[] args) {
-    WordSearch grid = new WordSearch(2,5);
-    System.out.println(grid);
-    /*Expected
-    _ _ _ _ _
-    _ _ _ _ _
-    */
-    System.out.println(grid.addWordHorizontal("hey", 0, 1)); //true;
-    grid.addWordHorizontal("hey", 0, 1);
-    System.out.println(grid);
-    /*Expected
-    _ h e y _
-    _ _ _ _ _
-    */
-    System.out.println(grid.addWordHorizontal("dogma", 0, 1)); //false;
-    grid.addWordHorizontal("dogma", 0, 1);
-    System.out.println(grid); //no change
-    System.out.println(grid.addWordHorizontal("hay", 0, 1)); //false;
-    grid.addWordHorizontal("hay", 0, 1);
-    System.out.println(grid); // no change
-    System.out.println(grid.addWordVertical("do", 0, 0)); //true
-    grid.addWordVertical("do", 0,0);
-    System.out.println(grid);
-    /*Expected
-    d h e y _
-    o _ _ _ _
-    */
-    System.out.println(grid.addWordVertical("ho", 0, 1)); //true
-    grid.addWordVertical("ho", 0,1);
-    System.out.println(grid);
-    /*Expected
-    d h e y _
-    o o _ _ _
-    */
-    System.out.println(grid.addWordVertical("hoo", 0, 1)); //false
-    grid.addWordVertical("hoo", 0,1);
-    System.out.println(grid); // no change
-    System.out.println(grid.addWordHorizontal("oomph", 1, 0)); //false
-    grid.addWordVertical("oomph", 1,0);
-    System.out.println(grid);
-    /*Expected
-    d h e y _
-    o o m p h
-    */
+    try {
+      WordSearch grid = new WordSearch(10,10, "words.txt");
+      System.out.println(grid);
+    }catch(FileNotFoundException e) {
+      System.out.println("File not found: " + "words.txt");
+      System.exit(1);
+    }
   }
 }
