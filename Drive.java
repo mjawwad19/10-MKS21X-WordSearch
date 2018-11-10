557017013f28844741b3a25e14c116da9fbb02f0
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-09 23:31:11 -0500

    Updated to fix typo that caused the seed given in terminal to be the column width

--- Drive.java
+++ Drive.java
@@ -32,27 +32,66 @@ public class Drive{
     }
   try {
     WordSearch grid0 = new WordSearch(10,10, "words.txt", 100);
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("venasaur", 1, 3, 1, 0); // should not do anything
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("ekans", 1, 4, 1, 0); //ekans vertically
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
-    //System.out.println(grid);
+    //System.out.println(grid0);
     grid0.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
-    //System.out.println(grid);
+    //System.out.println(grid0);
     // TECHNICALLY NOT IN MY WORDS TEXT
     grid0.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
-    //System.out.println(grid);
+    //System.out.println(grid0);
     // TECHNICALLY NOT IN MY WORDS TEXT
     grid0.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
     System.out.println(grid0);
+  }catch(FileNotFoundException e) {
+    System.out.println("File not found: " + "words.txt");
+    System.exit(1);
+  }
+  int defaultRow = 10;
+  int defaultCol = 10;
+  int defaultSeed = 0;
+  if (args.length > 0) {
+    defaultRow = Integer.parseInt(args[0]);
+  }
+  if (args.length > 1) {
+    defaultCol = Integer.parseInt(args[1]);
+  }
+  if (args.length > 3) {
+    defaultSeed = Integer.parseInt(args[3]);
+  }
+  try {
+    WordSearch grid1 = new WordSearch(defaultRow, defaultCol, "words.txt", defaultSeed);
+    //System.out.println(grid1);
+    grid1.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
+    //System.out.println(grid1);
+    grid1.addWord("venasaur", 1, 3, 1, 0); // should not do anything
+    //System.out.println(grid1);
+    grid1.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
+    //System.out.println(grid1);
+    grid1.addWord("ekans", 1, 4, 1, 0); //ekans vertically
+    //System.out.println(grid1);
+    grid1.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
+    //System.out.println(grid1);
+    grid1.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
+    //System.out.println(grid1);
+    grid1.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
+    //System.out.println(grid1);
+    // TECHNICALLY NOT IN MY WORDS TEXT
+    grid1.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
+    //System.out.println(grid1);
+    // TECHNICALLY NOT IN MY WORDS TEXT
+    grid1.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
+    System.out.println(grid1);
   }catch(FileNotFoundException e) {
     System.out.println("File not found: " + "words.txt");
     System.exit(1);
