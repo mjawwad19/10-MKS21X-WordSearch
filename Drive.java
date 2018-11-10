f6b2f834b405a2fd3eae013ec41d0ae0ff299eab
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-09 20:14:22 -0500

    Updated Drive.java to move tests to reg constructor creation and test negative direction

--- Drive.java
+++ Drive.java
@@ -5,18 +5,29 @@ public class Drive{
   public static void main(String[] args) {
     try {
       WordSearch grid = new WordSearch(10,10, "words.txt");
-      System.out.println(grid);
-      grid.addWordHorizontal("pikachu", 2, 2);
-      System.out.println(grid);
-      grid.addWordDiagonal("venasaur", 0, 0); // shoould not do anything
-      System.out.println(grid);
-      grid.addWordDiagonal("arbok", 2, 5);
-      System.out.println(grid);
-      grid.addWordVertical("ekans", 1, 4);
-      System.out.println(grid);
     }catch(FileNotFoundException e) {
       System.out.println("File not found: " + "words.txt");
       System.exit(1);
     }
-    }
+    WordSearch grid0 = new WordSearch(10,10);
+    //System.out.println(grid);
+    grid0.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
+    //System.out.println(grid);
+    grid0.addWord("venasaur", 1, 3, 1, 0); // should not do anything
+    //System.out.println(grid);
+    grid0.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
+    //System.out.println(grid);
+    grid0.addWord("ekans", 1, 4, 1, 0); //ekans vertically
+    //System.out.println(grid);
+    grid0.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
+    //System.out.println(grid);
+    grid0.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
+    //System.out.println(grid);
+    grid0.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
+    //System.out.println(grid);
+    grid0.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
+    //System.out.println(grid);
+    grid0.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
+    System.out.println(grid);
   }
+}
