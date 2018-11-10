9b1d5f19c0387137e8acd1e7b7bea0b5baa25bf0
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-09 19:26:07 -0500

    Updated my private helper to conform to Mr.K's private addWords but I have an issue with this: why is this method still private? There are no new instructions for addWordH,D,or Vertical so we can't choose the negative ability?

--- WordSearch.java
+++ WordSearch.java
@@ -83,67 +83,35 @@ public class WordSearch{
     takes all the previous return false conditionals;
     since the only difference between the add is the adding index of the word direction,
     a 0 or 1 can dictate if I want horizontal (0,1), vertical (1,0) or diagonal (1,1)*/
-    private boolean addWord(String word, int row, int col, int r, int c) {
+    private boolean addWord(int r, int c, String word, int rowIncrement, int colIncrement) {
       int len = word.length();
-      if (row < 0 ||
-          col < 0 ||
-          row >= height ||
-          col >= width) return false;
+      if (r < 0 ||
+          c < 0 ||
+          r >= height ||
+          c >= width ||
+          rowIncrement == 0 && colIncrement == 0) return false;
       // this if is from all addWords previously made nothing new
-      if (row + (len -1) * r < 0 || row + (len -1) * r >= height ||
-          col + (len -1) * c < 0 || col + (len - 1) * c >= width) return false;
+      if (r + (len -1) * rowIncrement < 0 || r + (len -1) * rowIncrement >= height ||
+          c + (len -1) * colIncrement < 0 || c + (len - 1) * colIncrement >= width) return false;
       // this is a combination of col + len > data[row] // row + len > data[col]
       for (int i = 0; i < len; i++) {
-        if (data[row +i*r] [col +i*c] != '_' &&
-            data[row +i*r] [col +i*c] != word.charAt(i)) return false;
+        if (data[r +i*rowIncrement] [c +i*colIncrement] != '_' &&
+            data[r +i*rowIncrement] [c +i*colIncrement] != word.charAt(i)) return false;
       }
       for (int i = 0; i < len; i++) {
-        data[row +i*r][col +i*c] = word.charAt(i);
+        data[r +i*rowIncrement][c +i*colIncrement] = word.charAt(i);
       }
+      wordsToAdd.remove(word);
+      wordsAdded.add(word);
       return true;
     }
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from left to right, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     * or there are overlapping letters that do not match, then false is returned
-     * and the board is NOT modified.
-     */
-    public boolean addWordHorizontal(String word,int row, int col){
-      return addWord(word, row, col, 0, 1);
+    public boolean addWordHorizontal(String word, int row, int col) {
+      return addWord(row, col, word, 0, 1);
     }
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from top to bottom, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned.
-     *and the board is NOT modified.
-     */
-    public boolean addWordVertical(String word,int row, int col){
-      return addWord(word, row, col, 1, 0);
+    public boolean addWordVertical(String word, int row, int col) {
+      return addWord(row, col, word, 1, 0);
     }
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-    *The word is added from top left to bottom right, must fit on the WordGrid,
-    *and must have a corresponding letter to match any letters that it overlaps.
-    *
-    *@param word is any text to be added to the word grid.
-    *@param row is the vertical locaiton of where you want the word to start.
-    *@param col is the horizontal location of where you want the word to start.
-    *@return true when the word is added successfully. When the word doesn't fit,
-    *or there are overlapping letters that do not match, then false is returned.
-    */
-   public boolean addWordDiagonal(String word,int row, int col){
-     return addWord(word, row, col, 1, 1);
-   }
-}
+    public boolean addWordDiagonal(String word, int row, int col) {
+      return addWord(row, col, word, 1, 1);
+    }
+  }
