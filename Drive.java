f0fe8c46f61a37e56a07461a83b68c4f3bc9f72d
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-10 15:42:13 -0500

    Updated drive to do A FULL SCREEN BIG GRID OF 801 POKEMON  IT LOOKS CRAY CRAY

--- Drive.java
+++ Drive.java
@@ -4,59 +4,20 @@ import java.io.*;
 public class Drive{
   public static void main(String[] args) {
     try {
-      WordSearch grid = new WordSearch(10,10, "words.txt");
-      //System.out.println(grid);
-      grid.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
-      //System.out.println(grid);
-      grid.addWord("venasaur", 1, 3, 1, 0); // should not do anything
-      //System.out.println(grid);
-      grid.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
-      //System.out.println(grid);
-      grid.addWord("ekans", 1, 4, 1, 0); //ekans vertically
-      //System.out.println(grid);
-      grid.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
-      //System.out.println(grid);
-      grid.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
-      //System.out.println(grid);
-      grid.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
-      //System.out.println(grid);
-      // TECHNICALLY NOT IN MY WORDS TEXT
-      grid.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
-      //System.out.println(grid);
-      // TECHNICALLY NOT IN MY WORDS TEXT
-      grid.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
+      WordSearch grid = new WordSearch(175,112, "pokemon.txt");
+      //this takes up the whole screen
       System.out.println(grid);
     }catch(FileNotFoundException e) {
-      System.out.println("File not found: " + "words.txt");
+      System.out.println("File not found: " + "pokemon.txt");
       System.exit(1);
     }
-  try {
-    WordSearch grid0 = new WordSearch(10,10, "words.txt", 100);
-    //System.out.println(grid0);
-    grid0.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
-    //System.out.println(grid0);
-    grid0.addWord("venasaur", 1, 3, 1, 0); // should not do anything
-    //System.out.println(grid0);
-    grid0.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
-    //System.out.println(grid0);
-    grid0.addWord("ekans", 1, 4, 1, 0); //ekans vertically
-    //System.out.println(grid0);
-    grid0.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
-    //System.out.println(grid0);
-    grid0.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
-    //System.out.println(grid0);
-    grid0.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
-    //System.out.println(grid0);
-    // TECHNICALLY NOT IN MY WORDS TEXT
-    grid0.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
-    //System.out.println(grid0);
-    // TECHNICALLY NOT IN MY WORDS TEXT
-    grid0.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
+  /*try {
+    WordSearch grid0 = new WordSearch(50,50, "words.txt", 100);
     System.out.println(grid0);
   }catch(FileNotFoundException e) {
     System.out.println("File not found: " + "words.txt");
     System.exit(1);
-  }
+  }*/
   int defaultRow = 10;
   int defaultCol = 10;
   int defaultSeed = 0;
@@ -70,30 +31,10 @@ public class Drive{
     defaultSeed = Integer.parseInt(args[3]);
   }
   try {
-    WordSearch grid1 = new WordSearch(defaultRow, defaultCol, "words.txt", defaultSeed);
-    //System.out.println(grid1);
-    grid1.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
-    //System.out.println(grid1);
-    grid1.addWord("venasaur", 1, 3, 1, 0); // should not do anything
-    //System.out.println(grid1);
-    grid1.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
-    //System.out.println(grid1);
-    grid1.addWord("ekans", 1, 4, 1, 0); //ekans vertically
-    //System.out.println(grid1);
-    grid1.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
-    //System.out.println(grid1);
-    grid1.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
-    //System.out.println(grid1);
-    grid1.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
-    //System.out.println(grid1);
-    // TECHNICALLY NOT IN MY WORDS TEXT
-    grid1.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
-    //System.out.println(grid1);
-    // TECHNICALLY NOT IN MY WORDS TEXT
-    grid1.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
+    WordSearch grid1 = new WordSearch(defaultRow, defaultCol, "pokemon.txt", defaultSeed);
     System.out.println(grid1);
   }catch(FileNotFoundException e) {
-    System.out.println("File not found: " + "words.txt");
+    System.out.println("File not found: " + "pokemon.txt");
     System.exit(1);
   }
 }
