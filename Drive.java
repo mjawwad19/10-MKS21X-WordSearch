40adb38caf25186ee1128347f6c32f93aaf4dc4c
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-09 19:26:59 -0500

    Updated personal driver to show add still works though I had to put it in the try statement or it would say I didn't do fileNotFound handling

--- Drive.java
+++ Drive.java
@@ -6,9 +6,17 @@ public class Drive{
     try {
       WordSearch grid = new WordSearch(10,10, "words.txt");
       System.out.println(grid);
+      grid.addWordHorizontal("pikachu", 2, 2);
+      System.out.println(grid);
+      grid.addWordDiagonal("venasaur", 0, 0); // shoould not do anything
+      System.out.println(grid);
+      grid.addWordDiagonal("arbok", 2, 5);
+      System.out.println(grid);
+      grid.addWordVertical("ekans", 1, 4);
+      System.out.println(grid);
     }catch(FileNotFoundException e) {
       System.out.println("File not found: " + "words.txt");
       System.exit(1);
     }
+    }
   }
-}
