c257ac5bf78df963ce21eb0dc8966e1a955d6ffb
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-09 20:22:43 -0500

    Disregard last commit moving tests outside of the try will yield an empty wordsToAdd arrayList so when I try adding it will attempt to remove from nothing causing NullPointerException

--- Drive.java
+++ Drive.java
@@ -5,29 +5,30 @@ public class Drive{
   public static void main(String[] args) {
     try {
       WordSearch grid = new WordSearch(10,10, "words.txt");
+      //System.out.println(grid);
+      grid.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
+      //System.out.println(grid);
+      grid.addWord("venasaur", 1, 3, 1, 0); // should not do anything
+      //System.out.println(grid);
+      grid.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
+      //System.out.println(grid);
+      grid.addWord("ekans", 1, 4, 1, 0); //ekans vertically
+      //System.out.println(grid);
+      grid.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
+      //System.out.println(grid);
+      grid.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
+      //System.out.println(grid);
+      grid.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
+      //System.out.println(grid);
+      // TECHNICALLY NOT IN MY WORDS TEXT
+      grid.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
+      //System.out.println(grid);
+      // TECHNICALLY NOT IN MY WORDS TEXT
+      grid.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
+      System.out.println(grid);
     }catch(FileNotFoundException e) {
       System.out.println("File not found: " + "words.txt");
       System.exit(1);
     }
-    WordSearch grid0 = new WordSearch(10,10);
-    //System.out.println(grid);
-    grid0.addWord("pikachu", 2, 2, 0, 1); //pikachu horizontally
-    //System.out.println(grid);
-    grid0.addWord("venasaur", 1, 3, 1, 0); // should not do anything
-    //System.out.println(grid);
-    grid0.addWord("arbok", 2, 5, 1, 1); //arbok diagonally
-    //System.out.println(grid);
-    grid0.addWord("ekans", 1, 4, 1, 0); //ekans vertically
-    //System.out.println(grid);
-    grid0.addWord("raichu", 7, 8, 0, -1); // raichu spelled backwards horizontally
-    //System.out.println(grid);
-    grid0.addWord("espeon", 9, 0, -1, 0); // espeon spelled backwards vertically;
-    //System.out.println(grid);
-    grid0.addWord("ekans", 9, 0, -1, 1); // ekans diagonal up right
-    //System.out.println(grid);
-    grid0.addWord("pan", 2, 2, 1, -1); //pan diagonal down left
-    //System.out.println(grid);
-    grid0.addWord("pun", 2, 2, -1, -1);; //pun diagonal up left
-    System.out.println(grid);
+    }
   }
-}
