d88260c8c4c72dea101b9e67852bf49fc68b7b97
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-10 20:08:27 -0500

    After being told that our DRIVERS WILL BE USED, I decided to have the fileInput be what the user puts in as a terminal argument

--- Drive.java
+++ Drive.java
@@ -21,26 +21,22 @@ public class Drive{
   int defaultRow = 10;
   int defaultCol = 10;
   int defaultSeed = 0;
-  if (args.length > 0) {
-    defaultRow = Integer.parseInt(args[0]);
-  }
-  if (args.length > 1) {
-    defaultCol = Integer.parseInt(args[1]);
-  }
-  if (args.length > 3) {
-    defaultSeed = Integer.parseInt(args[3]);
-  }
+  String fileName = "";
+  if (args.length > 0) defaultRow = Integer.parseInt(args[0]);
+  if (args.length > 1) defaultCol = Integer.parseInt(args[1]);
+  if (args.length > 2) fileName = args[2];
+  if (args.length > 3) defaultSeed = Integer.parseInt(args[3]);
   try {
     if (defaultSeed != 0) {
-      WordSearch grid1 = new WordSearch(defaultRow, defaultCol, "pokemon.txt", defaultSeed);
-    System.out.println(grid1);
+      WordSearch grid1 = new WordSearch(defaultRow, defaultCol, fileName, defaultSeed);
+      System.out.println(grid1);
   }
     else {
-      WordSearch grid1 = new WordSearch(defaultRow, defaultCol, "pokemon.txt");
+      WordSearch grid1 = new WordSearch(defaultRow, defaultCol, fileName);
       System.out.println(grid1);
     }
   }catch(FileNotFoundException e) {
-    System.out.println("File not found: " + "pokemon.txt");
+    System.out.println("File not found: " + fileName + " Please create one");
     System.exit(1);
   }
 }
