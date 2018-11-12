c6047a8b604fc90e8767f23b9ed95837c9f488a2
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-11 22:01:13 -0500

    For idiot users/ those that do not enter in the correct order*

--- Drive.java
+++ Drive.java
@@ -7,8 +7,11 @@ public class Drive{
   int defaultCol = 10;
   int defaultSeed = 0;
   String fileName = "";
+  System.out.println("If you wish to try again, please enter arguments in this order: row col fileName <optional> seed");
   if (args.length > 0) defaultRow = Integer.parseInt(args[0]);
+  else System.out.println("Please enter arguments in this order: row col fileName <optional> seed");
   if (args.length > 1) defaultCol = Integer.parseInt(args[1]);
+  else System.out.println("Please enter arguments in this order: row col fileName <optional> seed");
   if (args.length > 2) fileName = args[2];
   else {System.out.println("Please enter a file to use for the wordsearch in this order: row col fileName <optional> seed");}
   if (args.length > 3) defaultSeed = Integer.parseInt(args[3]);
@@ -26,5 +29,6 @@ public class Drive{
     System.out.println("File not found: " + fileName + " Please create one");
     System.exit(1);
   }
+  System.out.println("If you wish to try again, please enter arguments in this order: row col fileName <optional> seed");
 }
 }
