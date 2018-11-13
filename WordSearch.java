f018f5f378c5170ee7a5ca72fb04ae07e0c15354
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-13 11:11:25 -0500

    Moved main to WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -175,4 +175,36 @@ public class WordSearch{
         //remove a word from being chosen aaain if it cant be added ever
       }
     }
+    public static void main(String[] args) {
+    int defaultRow = 10;
+    int defaultCol = 10;
+    int defaultSeed = 0;
+    String fileName = "";
+    int answer = 0;
+    System.out.println("If you wish to try again, please enter arguments in this order: row col fileName <optional> seed <optional> answer \n\n\n");
+    if (args.length > 0) defaultRow = Integer.parseInt(args[0]);
+    else System.out.println("Please enter arguments in this order: row col fileName <optional> seed <optional> answer");
+    if (args.length > 1) defaultCol = Integer.parseInt(args[1]);
+    else System.out.println("Please enter arguments in this order: row col fileName <optional> seed <optional> answer");
+    if (args.length > 2) fileName = args[2];
+    else {System.out.println("Please enter a file to use for the wordsearch in this order: row col fileName <optional> seed <optional> answer");}
+    if (args.length > 3) defaultSeed = Integer.parseInt(args[3]);
+    if (args.length > 4 && (args[4].equals("key"))) answer = 1;
+    try {
+      if (args.length == 0 ) System.out.println("You have not inputted any terminal arguments for me to run a WordSearch. Please put in this order: row col fileName <optional> seed");
+      if (defaultSeed != 0) {
+        WordSearch grid1 = new WordSearch(defaultRow, defaultCol, fileName, defaultSeed);
+        System.out.println(grid1);
+    }
+      else {
+        WordSearch grid1 = new WordSearch(defaultRow, defaultCol, fileName);
+        System.out.println(grid1);
+      }
+    }catch(FileNotFoundException e) {
+      System.out.println("File not found: " + fileName + " Please create one");
+      System.exit(1);
+    }
+    System.out.println("If you wish to try again, please enter arguments in this order: row col fileName <optional> seed");
+  }
+
   }
